d0c097374e9173f31e0299cac15994df0f663fa1
arifmia1129 <arifmia1129@gmail.com>
2023-08-17 08:24:45 +0600

    initial setup commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-500">
+        Hello Everyone. Today I will discus about react redux
+      </h1>
+    </>
+  );
+}
+
+export default App;
